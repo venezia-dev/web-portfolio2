c6b44f017e8c710bef145b3a7b1c9753ab88bf39
leandro venezia <dev.venezia@gmail.com>
2020-01-20 20:57:38 -0300

    edit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,12 +9,12 @@ export class HomeComponent implements OnInit {
 
   //Manualmente hasta que instagram, libere un Widget!
   instagram = [
+    {"img": "https://www.instagram.com/p/B7j4hmShZmr"},
     {"img": "https://www.instagram.com/p/B270MlKhvXC"},
     {"img": "https://www.instagram.com/p/B2dBXZShXM4"},
     {"img": "https://www.instagram.com/p/B1ztlMEBpjq"},
     {"img": "https://www.instagram.com/p/Bw0a6SWhtGs"},
     {"img": "https://www.instagram.com/p/BtuKmNWBbnD"},
-    {"img": "https://www.instagram.com/p/BqbJGqXhU23"}
   ]
 
   constructor(
